2d85c2374ff991d4daa4055a32940494c8297a63
噗噗 <ethan42411@gmail.com>
2018-04-07 13:41:54 +0800

    Add Arduino-Read-internal-voltage.ino

---
+++ Arduino-Read-internal-voltage.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  double curvolt = double( readVcc() ) / 1000;
+  Serial.println(curvolt);
+  delay(100);
+}
+
+//read internal voltage
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  result = ADCL;
+  result |= ADCH << 8;
+  result = 1126400L / result; // Back-calculate AVcc in mV
+  return result;
+}
